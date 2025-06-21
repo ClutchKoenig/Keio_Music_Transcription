08e2d13f4891540305cd03bf9a2e58cdb073155b
ClutchKoenig <bastian.tellkamp@gmail.com>
2025-06-21 10:53:14 +0200

    First Test Case basic_pitch

---
+++ src/model/midi_generator.py
@@ -0,0 +1,44 @@
+from basic_pitch.inference import predict, predict_and_save, Model
+from basic_pitch import ICASSP_2022_MODEL_PATH
+# Prototype
+# predict_and_save(
+#     <input-audio-path-list>,
+#     <output-directory>,
+#     <save-midi>,
+#     <sonify-midi>,
+#     <save-model-outputs>,
+#     <save-notes>,
+# )
+#model_output, midi_data, note_events = predict("input/audio/file.wav")
+class audio_conversion():
+    def __init__(self, song:str = 'default_song', 
+                 audio_path: str = 'data/raw', 
+                 output_path: str = 'output/model_midi'):
+        
+        self.audio_path = f'{audio_path}/{song}/{song}.mp3'
+        self.output_path = output_path
+    
+    def conversion_1(self):
+        print("🔍 Lade Audio:", self.audio_path)
+        print("📂 Speichere nach:", self.output_path)
+
+        predict_and_save(audio_path_list=[self.audio_path], output_directory=self.output_path, 
+                         save_midi=True, sonify_midi=False, 
+                         save_model_outputs=True, save_notes=True,
+                         model_or_model_path=ICASSP_2022_MODEL_PATH)
+        
+
+modell = audio_conversion(song='OMORI_Final-Duet')
+modell.conversion_1()
+        
+
+
+
+# def convert_audio(song:str, audio_path: str, output_path: str):
+#     audio_path = 'data/raw/{song}.mp3'
+#     output_path = 'output/model_midi'
+
+    
+    
+    
+#convert_audio('OMORI_Final-Duet')
