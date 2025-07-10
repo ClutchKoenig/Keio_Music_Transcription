7937e9d7f1f3e5b37bc92281721f2259eee97dc9
Thomas Bauer <thomas.bauer@insa-rouen.fr>
2025-07-10 13:35:22 +0900

    split.py refactoring

--- src/utils/split.py
+++ src/utils/split.py
@@ -1,9 +1,11 @@
 import os
 import argparse
 
+
 NB_STEMS_DEFAULT = 5
 PROP_DECREASE_DEFAULT = 0.5
 RMS_DEFAULT = 0.005
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Split audio into stems using Spleeter.")
@@ -21,18 +23,20 @@ if __name__ == "__main__":
         print(f"Error: file '{args.file}' does not exist.")
         exit(1)
 
+
 from spleeter.separator import Separator
 import numpy as np
 import librosa
 import noisereduce as nr
 import soundfile as sf
 
+
 def split_audio(
-    file, 
-    nb_stems=NB_STEMS_DEFAULT, 
-    prop_decrease=PROP_DECREASE_DEFAULT, 
-    rms=RMS_DEFAULT,
-):
+        file, 
+        nb_stems=NB_STEMS_DEFAULT, 
+        prop_decrease=PROP_DECREASE_DEFAULT, 
+        rms=RMS_DEFAULT,
+    ):
     # Load the audio file
     y_mono, sr = librosa.load(file, sr=44100, mono=True)
 
@@ -71,10 +75,13 @@ def split_audio(
 
     return prediction
 
-def output_split(input_file: str, nb_stems=NB_STEMS_DEFAULT, 
-    prop_decrease=PROP_DECREASE_DEFAULT, 
-    rms=RMS_DEFAULT,
-):
+
+def output_split(
+        input_file: str, 
+        nb_stems=NB_STEMS_DEFAULT, 
+        prop_decrease=PROP_DECREASE_DEFAULT, 
+        rms=RMS_DEFAULT,
+    ):
     prediction = split_audio(input_file, nb_stems, prop_decrease, rms)
 
     filename = os.path.splitext(os.path.basename(input_file))[0]
@@ -87,16 +94,6 @@ def output_split(input_file: str, nb_stems=NB_STEMS_DEFAULT,
         print(f"Saved {stem} stem to {output_file}")
     print("================Audio splitting completed=====================")
 
+
 if __name__ == "__main__":
-    prediction = split_audio(args.file, args.nb_stems, args.prop_decrease, args.rms)
-
-    filename = os.path.splitext(os.path.basename(args.file))[0]
-    output_dir = f'output/{filename}'
-    os.makedirs(output_dir, exist_ok=True)
-
-    for stem, audio in prediction.items():
-        output_file = os.path.join(output_dir, f"{stem}.wav")
-        sf.write(output_file, audio, 44100)
-        print(f"Saved {stem} stem to {output_file}")
-
-    print("Audio splitting completed.")
+    output_split(args.file, args.nb_stems, args.prop_decrease, args.rms)
